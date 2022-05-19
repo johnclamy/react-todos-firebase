f06879516c15592572398611434cc81e7bd23d32
John_Lamy <johnclamy@gmail.com>
2022-05-19 16:15:14 +0100

    setup inital app data

---
+++ src/app/data.js
@@ -0,0 +1,10 @@
+const data = {
+  counter: 3,
+  todos: [
+    { id: 1, text: "walk the dog" },
+    { id: 2, text: "cook breakfast" },
+    { id: 3, text: "go to work" },
+  ],
+};
+
+export default data
