ba4912ba4ba2acaa6f86d721369bf3daa03d6f4e
John_Lamy <johnclamy@gmail.com>
2022-05-19 16:21:58 +0100

    AppHeader component

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,8 @@
+const AppHeader = () =>
+  <header className="mt-2 mb-4">
+    <h1 className="text-center text-capitalize text-info display-3">
+      todo app
+    </h1>
+  </header>
+
+export default AppHeader
