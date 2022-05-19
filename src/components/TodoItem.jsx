b1b2c612b98c4ed991e95f808e28e92d45df0f54
John_Lamy <johnclamy@gmail.com>
2022-05-19 16:23:40 +0100

    set render for each todo

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import { Button, Form, ListGroup } from "react-bootstrap";
+
+const TodoItem = ({ todo, removeItem, editItem }) => {
+  const MODES = ['listItem', 'editItem']
+  const [mode, setMode] = useState(MODES[0])
+  const [text, setText] = useState(todo.text)
+
+  const handleEditToSaveClick = () => {
+    editItem(text)
+    setMode(MODES[0])
+  }
+
+  const renderMode =
+    mode === MODES[0] ? (
+      <div className="d-flex justify-content-between align-items-center">
+        <span>{todo.text}</span>
+        <div>
+          <Button variant="danger" onClick={removeItem}>
+            Remove
+          </Button>
+          <Button
+            className="ml-2"
+            variant="warning"
+            onClick={() => setMode(MODES[1])}
+          >
+            Edit
+          </Button>
+        </div>
+      </div>
+    ) : (
+      <div>
+        <Form>
+          <Form.Control
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </Form>
+        <div>
+          <Button variant="dark" onClick={handleEditToSaveClick}>
+            Save
+          </Button>
+          <Button
+            className="ml-2"
+            variant="secondary"
+            onClick={() => setMode(MODES[0])}
+          >
+            Cancel
+          </Button>
+        </div>
+      </div>
+    );
+
+  return (
+    <ListGroup.Item className="border-info">
+      {renderMode}
+    </ListGroup.Item>
+  );
+}
+
+export default TodoItem
