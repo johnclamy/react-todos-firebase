58616adfe9523e9b68508837d4cf58624f2e043a
John_Lamy <johnclamy@gmail.com>
2022-05-29 08:08:54 +0100

    add media queries setup to theme/index.js

--- src/utils/theme/index.js
+++ src/utils/theme/index.js
@@ -1,35 +1,35 @@
 const theme = {
   color: {
     // Base palette colours
-    black: '#000',
-    darkGrey: '#333',
-    grey: '#ccc',
-    lightGrey: '#ebebeb',
-    offWhite: '#f5f5f5',
-    white: '#fff',
-    blue1: '#3498db',
-    blue2: '#2980b9',
-    red1: '#e74c3c',
-    red2: '#c0392b',
-    yellow1: '#f1c40f',
-    yellow2: '#f39c12',
-    green1: '#2ecc71',
-    green2: '#27ae60',
-    orange1: '#e67e22',
-    orange2: '#d35400',
-    aqua1: '#1abc9c',
-    aqua2: '#16a085',
-    purple1: '#9b59b6',
-    purple2: '#8e44ad',
-    navy1: '#34495e',
-    navy2: '#2c3e50',
+    black: "#000",
+    darkGrey: "#333",
+    grey: "#ccc",
+    lightGrey: "#ebebeb",
+    offWhite: "#f5f5f5",
+    white: "#fff",
+    blue1: "#3498db",
+    blue2: "#2980b9",
+    red1: "#e74c3c",
+    red2: "#c0392b",
+    yellow1: "#f1c40f",
+    yellow2: "#f39c12",
+    green1: "#2ecc71",
+    green2: "#27ae60",
+    orange1: "#e67e22",
+    orange2: "#d35400",
+    aqua1: "#1abc9c",
+    aqua2: "#16a085",
+    purple1: "#9b59b6",
+    purple2: "#8e44ad",
+    navy1: "#34495e",
+    navy2: "#2c3e50",
 
     // App colours
-    appBlue: '#2B2D42',
-    appGrey: '#8D99AE',
-    appLight: '#EDF2F4',
-    appRed: '#EF233C',
-    appCrimson: '#D90429',
+    appBlue: "#2B2D42",
+    appGrey: "#8D99AE",
+    appLight: "#EDF2F4",
+    appRed: "#EF233C",
+    appCrimson: "#D90429",
   },
 
   // Backgrounds
@@ -39,7 +39,7 @@ const theme = {
     third: this.color.appGrey,
     inverse: this.color.appBlue,
   },
-  
+
   // Text
   text: {
     primary: this.color.appBlue,
@@ -51,14 +51,14 @@ const theme = {
   // Links
   link: {
     primary: this.color.grey,
-    primaryHover: this.color.appBlue
+    primaryHover: this.color.appBlue,
   },
 
   // Borders
   border: {
     color: this.color.grey,
-    size: '1px',
-    type: 'solid',
+    size: "1px",
+    type: "solid",
     focus: this.color.grey,
     colorOther: this.color.appCrimson,
   },
@@ -67,15 +67,24 @@ const theme = {
   font: {
     familyText: `'Poppins', sans-serif;`,
     familyHeading: `'Lobster', cursive;`,
-    baseSize: '14px',
-    baseLineHeight: '1.5',
+    baseSize: "14px",
+    baseLineHeight: "1.5",
   },
 
   // Layout
   layout: {
-    margin: '1rem',
-    padding: '1rem',
+    margin: "1rem",
+    padding: "1rem",
   },
-}
+
+  // Media queries
+  mediaQuery: {
+    xs: `only screen and (max-width: 25em)`,
+    sm: `only screen and (max-width: 37.5em)`,
+    md: `only screen and (max-width: 56em)`,
+    lg: `only screen and (max-width: 80em)`,
+    xl: `only screen and (max-width: 90em)`,
+  },
+};
 
 export default theme
