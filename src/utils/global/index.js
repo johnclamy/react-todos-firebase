d7ff0f4752c6fba9f663685e5989f458d70f0d1c
John_Lamy <johnclamy@gmail.com>
2022-05-30 22:30:58 +0100

    add background and text colours

--- src/utils/global/index.js
+++ src/utils/global/index.js
@@ -22,23 +22,38 @@ export default createGlobalStyle`
     box-sizing: border-box;
     
     // App colour palette
-    --primary: ${props => props.theme.color.navy3};
-    --secondary: ${props => props.theme.color.lightGrey};
-    --third: ${props => props.theme.color.red2};
-    --dark: ${props => props.theme.color.darkGrey};
-    --light: ${props => props.theme.color.lighterGrey};
-    --info: ${props => props.theme.color.blue1};
-    --danger: ${props => props.theme.color.red1};
-    --warning: ${props => props.theme.color.yellow2};
-    --success: ${props => props.theme.color.green1};
-    --shadow: ${props => props.theme.color.shadow};
+    --primary: ${(props) => props.theme.color.navy3};
+    --secondary: ${(props) => props.theme.color.lightGrey};
+    --third: ${(props) => props.theme.color.red2};
+    --dark: ${(props) => props.theme.color.darkGrey};
+    --light: ${(props) => props.theme.color.lighterGrey};
+    --info: ${(props) => props.theme.color.blue1};
+    --danger: ${(props) => props.theme.color.red1};
+    --warning: ${(props) => props.theme.color.yellow2};
+    --success: ${(props) => props.theme.color.green1};
+    --shadow: ${(props) => props.theme.color.shadow};
+
+    // Backgrounds
+    --primary-bg: ${(props) => props.theme.color.lighterGrey};
+    --secondary-bg: ${(props) => props.theme.color.offWhite};
+    --loud-bg: ${(props) => props.theme.color.red2};
+    --inverse-bg: ${(props) => props.theme.color.navy3};
+
+    // Text
+    --primary-text: ${(props) => props.theme.color.darkGrey};
+    --secondary-text: ${(props) => props.theme.color.navy3};
+    --inverse-text: ${(props) => props.theme.color.white};
+    --loud-text: ${(props) => props.theme.color.black};
+
+    // Borders
+    
 
     // Media queries
-    @media ${props => props.theme.media.xs} {
+    @media ${(props) => props.theme.media.xs} {
       font-size: 55%;
     }
 
-    @media ${props => props.theme.media.sm} {
+    @media ${(props) => props.theme.media.sm} {
       font-size: 60%;
     }
   }
@@ -58,4 +73,38 @@ export default createGlobalStyle`
     text-decoration: none;
     font-family: inherit;
   }
+
+  a, a:link, a:visited {
+    color: #333;
+  }
+
+  a:hover {
+    color: #d90429;
+  }
+
+  h1 {
+    color: black;
+    font-family: 'Lobster', cursive;
+    font-size: 4.8rem;
+  }
+
+  h2 {
+    color: #d90429;
+    font-weight: 400;
+    font-size: 2.2rem;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+  }
+
+  h3, p {
+    color: #2b2d42;
+    font-weight: 400;
+  }
+
+  p {
+    font-size: 1.3rem;
+  }
+
 `;
