44fc70786ea5eab9f6e535f5cac5b50a9eed7ddf
John_Lamy <johnclamy@gmail.com>
2022-05-31 16:25:40 +0100

    setup the app logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,16 @@
-import { Title } from '../utils/styles'
+import styled from 'styled-components';
 
-const Logo = () => <Title>my todos</Title>
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  font-family: "Lobster", cursive;
+  font-weight: 700;
+  font-size: 2.8rem;
+  padding: 1rem;
+  color: var(--third);
+  height: 100%;
+`;
 
-export default Logo
+const Logo = () => <Title>My Todos</Title>;
+
+export default Logo;
