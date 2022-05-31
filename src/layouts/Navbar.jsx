0ca363e4737f0fab4747fd9557bfb84b2baf1e31
John_Lamy <johnclamy@gmail.com>
2022-05-31 16:23:58 +0100

    set up the navbar in Navbar.jsx

---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+
+import { Container } from '../utils/styles'
+import Logo from '../components/Logo'
+import NavLinks from '../components/NavLinks'
+
+const Wrapper = styled.div`
+  position: fixed;
+  background-color: var(--secondary-bg);
+  padding: 0 2rem;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 6rem;
+
+  @media ${(prosp) => prosp.theme.media.xs} {
+    display: none;
+  }
+`;
+
+const NavItems = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: space-between;
+`;
+
+const Navbar = () => {
+  return (
+    <Wrapper>
+      <Container>
+        <NavItems>
+          <Logo />
+          <NavLinks />
+        </NavItems>
+      </Container>
+    </Wrapper>
+  );
+}
+
+export default Navbar
