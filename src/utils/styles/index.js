0f23bf832ecd244d2391822a390d4c1f9910ed0e
John_Lamy <johnclamy@gmail.com>
2022-05-31 11:38:35 +0100

    Title styled component for Logo.jsx

---
+++ src/utils/styles/index.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+// components/Logo.jsx
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  font-family: 'Lobster', cursive;
+  font-weight: 700;
+  font-size: 2.8rem;
+  text-decoration: uppercase;
+  padding: 1rem;
+  color: var(--third);
+  height: 100%;
+`;
+
+export {
+  Title
+}
