fb40a2b0345030fd86b94113278e588fb51e9694
John_Lamy <johnclamy@gmail.com>
2022-06-01 10:33:48 +0100

    redirect to home page when logo gets clicked

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,16 +1,22 @@
 import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
 
 const Title = styled.div`
   display: flex;
   align-items: center;
   font-family: "Lobster", cursive;
   font-weight: 700;
+  color: var(--third);
+  cursor: pointer;
   font-size: 2.8rem;
   padding: 1rem;
-  color: var(--third);
   height: 100%;
 `;
 
-const Logo = () => <Title>My Todos</Title>;
+const Logo = () => {
+  const history = useHistory()
+
+  return <Title onClick={() => history.push('/')}>My Todos</Title>;
+}
 
 export default Logo;
