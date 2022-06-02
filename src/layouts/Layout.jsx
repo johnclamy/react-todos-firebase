8c1cf527ee16d8cc7b6aa50b1f567789d654cac6
John_Lamy <johnclamy@gmail.com>
2022-06-02 08:24:32 +0100

    add the SideDrawer to Layout.jsx

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,9 +1,11 @@
 import { Main } from '../utils/styles'
 import Navbar from './Navbar'
+import SideDrawer from './SideDrawer'
 
 const Layout = ({ children }) => (
   <>
     <Navbar />
+    <SideDrawer />
     <Main>{children}</Main>
   </>
 )
