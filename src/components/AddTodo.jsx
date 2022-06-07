be1390bc56d2f4f5c96dfcb7400918f78c6a4170
John_Lamy <johnclamy@gmail.com>
2022-06-07 12:41:15 +0100

    call the helper func to save todos in AddTodo.jsx

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import { StyledForm } from '../assets/styles/Form.styled'
+import { saveAndRender } from '../helper'
 
 const AddTodo = ({ todos = [], onSetTodos = f => f }) => {
-  const [task, setTask] = useState('')
+  const [task, setTask] = useState('');
 
   const handleSubmit = e => { 
-    e.preventDefault()
-    const id = todos.length ? todos[todos.length - 1].id + 1 : 1
-    onSetTodos([...todos, { id, checked: false, task }])
-    setTask('')
+    e.preventDefault();
+    const id = todos.length ? todos[todos.length - 1].id + 1 : 1;
+    const newTodos = [...todos, { id, checked: false, task }];
+    saveAndRender(onSetTodos, 'todos', newTodos);
+    setTask('');
   }
  
   return (
