f731da44e5690579bb0154b076f67fc47d56f84f
John_Lamy <johnclamy@gmail.com>
2022-06-07 14:07:40 +0100

    setup a title component for page sections

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import { StyledTitle } from '../assets/styles/Title.styled'
+
+const Title = () => (
+  <header>
+    <StyledTitle>
+      add a todo
+      <hr />
+    </StyledTitle>
+  </header>
+)
+
+export default Title
