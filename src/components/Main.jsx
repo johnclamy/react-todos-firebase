cd8dc1df9da3a39952a485be0b2d70ffeb7ab3b7
John_Lamy <johnclamy@gmail.com>
2022-06-07 19:02:40 +0100

    add search to Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,12 +3,14 @@ import { StyledMain } from '../assets/styles/Main.styled';
 import Todos from './Todos'
 import Stats from './Stats'
 import AddTodo from './AddTodo'
+import Search from "./Search";
 
 const Main = () => {
   const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')));
 
   return (
     <StyledMain>
+      <Search todos={todos} onSetTodos={setTodos} />
       <AddTodo todos={todos} onSetTodos={setTodos} />
       <Todos todos={todos} onSetTodos={setTodos} />
       <Stats todos={todos} />
