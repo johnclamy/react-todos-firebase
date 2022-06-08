4bf75668d44dfb9582d4587438f0a5cffc459de8
John_Lamy <johnclamy@gmail.com>
2022-06-08 16:28:21 +0100

    fetchData as a helper func

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,4 +1,23 @@
 const isSearched = term => item =>
   !term || item.task.toLowerCase().includes(term.toLowerCase())
 
-export { isSearched, }
+const fetchData = async (url, setData, setError, setIsLoading) => {
+  try {
+    const resp = await fetch(url);
+    if (!resp.ok) {
+      throw Error("Data not received for server");
+    }
+    const rslt = await resp.json();
+    setData(rslt);
+    setError('');
+  } catch (err) {
+    setError(err.message);
+  } finally {
+    setIsLoading(false)
+  }
+};
+
+export {
+  fetchData,
+  isSearched,
+};
