5cfaa12c5b7df9fc4347a9255b072b23cc5a86cb
John_Lamy <johnclamy@gmail.com>
2022-06-08 16:32:18 +0100

    fetch data in Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,36 +1,45 @@
 import { useState, useEffect } from "react";
+import { StyledLoading } from '../assets/styles/Loading.styles';
 import { StyledMain } from '../assets/styles/Main.styled';
-import { isSearched } from '../helper'
+import { StyledError } from "../assets/styles/Error.styled";
+import { fetchData, isSearched } from '../helper'
+import { TODOS_API_URL } from "../constants";
 import Todos from './Todos'
 import Stats from './Stats'
 import AddTodo from './AddTodo'
 import Search from "./Search";
 
 const Main = () => {
+  const [todos, setTodos] = useState([]);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("");
-  const [todos, setTodos] = useState(
-    JSON.parse(localStorage.getItem("todos")) || []
-  );
 
+  const renderApp = error ?
+    <StyledError>{error}</StyledError>
+    : (isLoading ?
+      <StyledLoading>Loading data...</StyledLoading>
+      : (
+      <>      
+        <Search searchTerm={searchTerm} onSetSearchTerm={setSearchTerm} />
+        <AddTodo todos={todos} onSetTodos={setTodos} />
+        <Todos
+          todos={todos.filter(isSearched(searchTerm))}
+          onSetTodos={setTodos}
+        />
+        <Stats todos={todos} />
+      </>
+      )
+    );
+
+  // Load data from RESTful API on initialization
   useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos]
-  )
+    setTimeout(() => {
+      fetchData(TODOS_API_URL, setTodos, setError, setIsLoading);
+    }, 2000)
+  }, [])
 
-  return (
-    <StyledMain>
-      <Search
-        searchTerm={searchTerm}
-        onSetSearchTerm={setSearchTerm}
-      />
-      <AddTodo todos={todos} onSetTodos={setTodos} />
-      <Todos
-        todos={todos.filter(isSearched(searchTerm))}
-        onSetTodos={setTodos}
-      />
-      <Stats todos={todos} />
-    </StyledMain>
-  );
+  return <StyledMain>{renderApp}</StyledMain>;
 }
 
 export default Main
