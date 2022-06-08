4abb2b69686609c001083b1df8986eb78fae578c
John_Lamy <johnclamy@gmail.com>
2022-06-08 16:29:00 +0100

    style the loading component

---
+++ src/assets/styles/Loading.styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const StyledLoading = styled.p`
+  border-top: 1px solid green;
+  border-bottom: 1px solid green;
+  padding: 0.5rem 0;
+  color: darkgreen;
+  font-weight: 200;
+  font-style: italic;
+  word-spacing: 3px;
+  letter-spacing: 1px;
+`
