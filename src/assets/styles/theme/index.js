1968cb0074fc12926ec3d0c57f9808ca4f252992
John_Lamy <johnclamy@gmail.com>
2022-06-29 22:14:50 +0100

    setup a theme

---
+++ src/assets/styles/theme/index.js
@@ -0,0 +1,29 @@
+const theme = {
+  colors: {
+    // base colours
+    black: "#000",
+    darkGray: "#282828",
+    gray: "#606060",
+    lightGray: "#ADADAD",
+    lighterGray: "#D5D5D5",
+    lightestGray: "#E9E9E9",
+    white: "#FFF",
+
+    // app colour palette
+    darkBlue: "#1D3557",
+    blue: "#457B9D",
+    lightBlue: "#A8DADC",
+    lighterBlue: "#F1FAEE",
+    red: "#E63946",
+  },
+
+  // media queries
+  media: {
+    xs: "(max-width: 320px)",
+    sm: "(max-width: 550px)",
+    md: "(max-width: 768px)",
+    lg: "(max-width: 960px)",
+  },
+};
+
+export default theme
