ee91c61166121c9194c1b0eab033712aa630812f
John_Lamy <johnclamy@gmail.com>
2022-06-29 22:15:55 +0100

    add ThemeProvider to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import { render } from 'react-dom'
+import { ThemeProvider } from 'styled-components'
+
 import App from './app/App'
+import GlobalStyles from './assets/styles/global'
+import theme from "./assets/styles/theme";
 
 const root = document.getElementById('root')
 
-render(<App />, root)
+render(
+  <ThemeProvider theme={theme}>
+    <App />
+    <GlobalStyles />
+  </ThemeProvider>,
+  root
+);
