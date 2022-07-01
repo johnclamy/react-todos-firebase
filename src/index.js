30117927cf2d0b6b1734a88b0eb32c7bb2d8796e
John_Lamy <johnclamy@gmail.com>
2022-07-02 00:48:37 +0100

    add BrowserRouter to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { render } from 'react-dom'
 import { ThemeProvider } from 'styled-components'
-
+import { BrowserRouter } from 'react-router-dom'
 import App from './app/App'
 import GlobalStyles from './assets/styles/global'
 import theme from "./assets/styles/theme";
@@ -8,9 +8,11 @@ import theme from "./assets/styles/theme";
 const root = document.getElementById('root')
 
 render(
-  <ThemeProvider theme={theme}>
-    <App />
-    <GlobalStyles />
-  </ThemeProvider>,
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyles />
+    </ThemeProvider>
+  </BrowserRouter>,
   root
 );
