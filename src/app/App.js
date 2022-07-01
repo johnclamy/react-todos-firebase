8b673a005b47ab9b2416b84dee0ba46339fb0e24
John_Lamy <johnclamy@gmail.com>
2022-07-02 00:56:59 +0100

    layout the routes in App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,16 @@
+import { Route, Switch, Redirect } from 'react-router-dom'
+import Layout from '../components/Layout'
+import HomePage from '../pages/Home'
+import TodosPage from '../pages/Todos'
+
 export default function App() { 
   return (
-    <>
-      test app component...
-    </>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/todos" component={TodosPage} />
+        <Redirect to='/' />
+      </Switch>
+    </Layout>
   );
 }
