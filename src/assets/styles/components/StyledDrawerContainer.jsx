c20eea0d650ff519de1d03b9b8a30e342d27e059
John_Lamy <johnclamy@gmail.com>
2022-07-02 00:53:08 +0100

    setup the drawer container

---
+++ src/assets/styles/components/StyledDrawerContainer.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const StyledDrawerContainer = styled.section`
+  position: fixed;
+  background-color: var(--primary);
+  padding: 0rem 2rem;
+  z-index: 10;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 6rem;
+  display: none;
+
+  @media ${({theme}) => theme.media.xs} {
+    display: flex;
+  }
+`;
+
+export default StyledDrawerContainer
