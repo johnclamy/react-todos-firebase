1ea65452eba4ae3f2b4379a3742d4d0f00e32aa8
John_Lamy <johnclamy@gmail.com>
2022-07-06 15:53:11 +0100

    add footer to layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,21 @@
+import StyledWrapper from "../assets/styles/components/StyledWrapper";
 import StyledMain from "../assets/styles/components/StyledMain";
+import StyledFooterWrapper from "../assets/styles/components/StyledFooterWrapper";
 import Navbar from "./Navbar"
 import Drawer from "./Drawer"
+import Footer from "./Footer";
 
 const Layout = ({ children }) => (
   <>
-    <Navbar />
-    <Drawer />
-    <StyledMain>{children}</StyledMain>
+    <StyledWrapper>
+      <Navbar />
+      <Drawer />
+      <StyledMain>{children}</StyledMain>
+    </StyledWrapper>
+    <StyledFooterWrapper>
+      <Footer />
+    </StyledFooterWrapper>
   </>
-)
+);
 
 export default Layout
