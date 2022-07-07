11ed561cd101d1c4589c5414bb6251f532a44056
John_Lamy <johnclamy@gmail.com>
2022-07-07 16:28:30 +0100

    add warning btn to btns lib

---
+++ src/assets/styles/components/StyledButtonWarning.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import StyledButton from "./StyledButton";
+
+const StyledButtonWarning = styled(StyledButton)`
+  color: var(--white);
+  background-color: var(--warning);
+`;
+
+export default StyledButtonWarning;
---
+++ src/components/ButtonWarning.jsx
@@ -0,0 +1,7 @@
+import StyledButtonWarning from "../assets/styles/components/StyledButtonWarning";
+
+const ButtonWarning = ({ children }) => (
+  <StyledButtonWarning>{children}</StyledButtonWarning>
+);
+
+export default ButtonWarning;
