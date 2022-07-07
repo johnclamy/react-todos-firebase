a76996b911e1a3b7bade4f6151c97eb4bca76836
John_Lamy <johnclamy@gmail.com>
2022-07-07 16:26:58 +0100

    add info btn to btns lib

---
+++ src/assets/styles/components/StyledButtonInfo.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import StyledButton from "./StyledButton";
+
+const StyledButtonInfo = styled(StyledButton)`
+  color: var(--darkGray);
+  background-color: var(--light);
+`;
+
+export default StyledButtonInfo
---
+++ src/components/ButtonInfo.jsx
@@ -0,0 +1,7 @@
+import StyledButtonInfo from "../assets/styles/components/StyledButtonInfo";
+
+const ButtonInfo = ({ children }) => (
+  <StyledButtonInfo>{children}</StyledButtonInfo>
+);
+
+export default ButtonInfo;
