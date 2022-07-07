91577918842f4a59ba28ad23f7f7db52a1583e7a
John_Lamy <johnclamy@gmail.com>
2022-07-07 16:27:48 +0100

    add btn light to btns lib

---
+++ src/assets/styles/components/StyledButtonLight.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import StyledButton from "./StyledButton";
+
+const StyledButtonLight = styled(StyledButton)`
+  color: var(--darkGray);
+  background-color: var(--lighter);
+`;
+
+export default StyledButtonLight;
---
+++ src/components/ButtonLight.jsx
@@ -0,0 +1,7 @@
+import StyledButtonLight from "../assets/styles/components/StyledButtonLight";
+
+const ButtonLight = ({ children }) => (
+  <StyledButtonLight>{children}</StyledButtonLight>
+);
+
+export default ButtonLight;
