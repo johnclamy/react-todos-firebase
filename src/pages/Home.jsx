576b728a535630738b47596c02aa2bcff60dd6de
John_Lamy <johnclamy@gmail.com>
2022-07-09 23:32:45 +0100

    test the rendering of the button components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,28 @@
-const Home = () => <>Home page...</>;
+import Button from '../components/Button'
+import ButtonInfo from '../components/ButtonInfo'
+import ButtonLight from '../components/ButtonLight'
+import ButtonWarning from '../components/ButtonWarning'
+
+const Home = () => {
+  return (
+    <>
+      <p style={{ marginBottom: "0.5rem" }}>
+        <Button>sign in</Button>
+      </p>
+      <p style={{ marginBottom: "0.5rem" }}>
+        <Button primary>sign in</Button>
+      </p>
+      <p style={{ marginBottom: "0.5rem" }}>
+        <ButtonInfo>sign in</ButtonInfo>
+      </p>
+      <p style={{ marginBottom: "0.5rem" }}>
+        <ButtonLight>sign in</ButtonLight>
+      </p>
+      <p style={{ marginBottom: "0.5rem" }}>
+        <ButtonWarning>sign in</ButtonWarning>
+      </p>
+    </>
+  );
+}
 
 export default Home;
