991a44090f180d0934a4c8a0a31d097a3cee43ba
John_Lamy <johnclamy@gmail.com>
2022-07-11 16:00:51 +0100

    setup an intro banner for Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,17 @@
-import Button from '../components/Button'
-import ButtonInfo from '../components/ButtonInfo'
-import ButtonLight from '../components/ButtonLight'
-import ButtonWarning from '../components/ButtonWarning'
+import StyledBanner from '../assets/styles/components/StyledBanner'
+import StyledBannerWrapper from '../assets/styles/components/StyledBannerWrapper';
+import StyledContainer from '../assets/styles/components/StyledContainer'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
-    <>
-      <p style={{ marginBottom: "0.5rem" }}>
-        <Button>sign in</Button>
-      </p>
-      <p style={{ marginBottom: "0.5rem" }}>
-        <Button primary>sign in</Button>
-      </p>
-      <p style={{ marginBottom: "0.5rem" }}>
-        <ButtonInfo>sign in</ButtonInfo>
-      </p>
-      <p style={{ marginBottom: "0.5rem" }}>
-        <ButtonLight>sign in</ButtonLight>
-      </p>
-      <p style={{ marginBottom: "0.5rem" }}>
-        <ButtonWarning>sign in</ButtonWarning>
-      </p>
-    </>
+    <StyledContainer>
+      <StyledBannerWrapper>
+        <StyledBanner>
+          <Banner />
+        </StyledBanner>
+      </StyledBannerWrapper>
+    </StyledContainer>
   );
 }
 
