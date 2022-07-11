98fffc659374dfefc7d4d8c0e53890ddc5304080
John_Lamy <johnclamy@gmail.com>
2022-07-11 16:10:08 +0100

    set a title component for the banner

---
+++ src/assets/styles/components/StyledBannerTitle.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const StyledBannerTitle = styled.h1`
+  color: var(--darkGray);
+  font-weight: 400;
+  font-size: 2.5rem;
+  text-align: center;
+  text-transform: capitalize;
+  margin-bottom: 3rem;
+
+  b {
+    color: var(--black);
+    font-weight: 700;
+  }
+`;
+
+export default StyledBannerTitle
